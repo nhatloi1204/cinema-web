24b22a66added51857a319567a8cd2c1113b0add
Nhat Loi <nhatloi120402@gmail.com>
2025-03-26 12:12:28 +0700

    config Route

---
+++ src/constants/pathNames.ts
@@ -0,0 +1,19 @@
+const pathNames = {
+  HOME: 'Home',
+  LOGIN: 'Login',
+  MOVIES: 'Movies',
+  MOVIE_DETAILS: 'Movie Details',
+  NEWS: 'News',
+  EVENTS: 'Events',
+  ABOUT_US: 'About Us',
+  PROFILE: 'Profile',
+
+  // Admin Routes (Commented for now)
+  // ADMIN_DASHBOARD: "Admin Dashboard",
+  // ADMIN_USERS: "Manage Users",
+  // ADMIN_MOVIES: "Manage Movies",
+
+  NOT_FOUND: '404 - Not Found',
+}
+
+export default pathNames
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import MainLayout from '../layouts/MainLayout'
-import Home from '../pages/Home'
+import userRoutes from './userRoutes'
+import adminRoutes from './adminRoutes'
 import NotFound from '../pages/NotFound'
 
 const AppRoutes = () => {
@@ -8,7 +9,21 @@ const AppRoutes = () => {
     <Router>
       <MainLayout>
         <Routes>
-          <Route path='/' element={<Home />} />
+          {/* User Routes */}
+          {userRoutes.map(({ path, component: Component }) => (
+            <Route key={path} path={path} element={<Component />} />
+          ))}
+
+          {/* Admin Routes */}
+          {/* {adminRoutes.map(({ path, component: Component, meta }) => (
+            meta.adminOnly ? (
+              <Route key={path} path={path} element={<AdminProtectedRoute><Component /></AdminProtectedRoute>} />
+            ) : (
+              <Route key={path} path={path} element={<Component />} />
+            )
+          ))} */}
+
+          {/* 404 Page */}
           <Route path='*' element={<NotFound />} />
         </Routes>
       </MainLayout>
