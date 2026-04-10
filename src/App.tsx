1c7aa3f1c36bd6629ab0e2ada7f05fbdda43ae99
Nhat Loi <nhatloi120402@gmail.com>
2026-04-10 14:13:10 +0700

    feat: implement user profile fetching on app initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import AppRoutes from './routes/AppRoutes'
 import { useInitializeApp } from './hooks/useInitializeApp'
+import { useAppDispatch } from './store/hooks'
+import { useEffect } from 'react'
+import { fetchProfile } from './store/userData/userThunk'
 
 function App() {
   // Initialize app - auto fetch user profile khi app load
   useInitializeApp()
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(fetchProfile())
+  }, [dispatch])
 
   return (
     <>
